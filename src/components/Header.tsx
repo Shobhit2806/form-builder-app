9acc5d715cfe9812c381840778b70fa7dbe209ec
Shobhit Garg <shobhit1256@gmail.com>
2025-02-25 12:34:23 +0530

    landing page added

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="bg-blue-900 text-white p-4 shadow-md h-[8vh]">
+      <h1 className="text-2xl font-bold font-serif">Form-Builder-App</h1>
+    </div>
+  );
+};
+
+export default Header;
