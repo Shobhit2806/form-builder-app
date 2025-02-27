eb8639d8fe8a1d8d29d203d3379ae4a636b6108f
Shobhit Garg <shobhit1256@gmail.com>
2025-02-27 21:22:28 +0530

    chore: vite config changes for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import FormBuilder from "./pages/Formbuilder";
+import FormBuilder from "./pages/FormBuilder";
 import FormRender from "./pages/FormRender";
 import LandingPage from "./pages/LandingPage";
 
